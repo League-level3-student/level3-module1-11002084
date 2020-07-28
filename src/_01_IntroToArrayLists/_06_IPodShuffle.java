32c96238481cfd2d37dfdeefae636b2b1f6159a1
11002084 <36611325+11002084@users.noreply.github.com>
2020-07-28 15:35:31 -0700

    7/28/2020
    Finished Ipod shuffle, started on Integer Stack

--- src/_01_IntroToArrayLists/_06_IPodShuffle.java
+++ src/_01_IntroToArrayLists/_06_IPodShuffle.java
@@ -16,18 +16,29 @@ public class _06_IPodShuffle implements ActionListener{
 	JFrame frame = new JFrame();
 	JPanel panel = new JPanel();
 	JButton playButton = new JButton();
+	JButton stopButton = new JButton();
+	
+	int selectedSongIndex = -1;
+	Random rand = new Random();
 	
 	ArrayList<Song> songList = new ArrayList<Song>();
 	
 	public _06_IPodShuffle() {
 		// 1. Use the Song class the play the demo.mp3 file.
-				songList.add(new Song(""));
+				songList.add(new Song("Rick Roll.mp3"));
+				songList.add(new Song("Megalovania.mp3"));
+				songList.add(new Song("All Star.mp3"));
+				songList.add(new Song("Despacito.mp3"));
 		
 				playButton.setText("Surprise me!");
 				playButton.addActionListener(this);
+				stopButton.setText("Stop Music");
+				stopButton.addActionListener(this);
 				panel.add(playButton);
+				panel.add(stopButton);
 				frame.add(panel);
 				frame.setTitle("Surprise Music Player");
+				frame.pack();
 				frame.setVisible(true);
 				
 		/**
@@ -41,15 +52,24 @@ public class _06_IPodShuffle implements ActionListener{
 	}
 	
 	public static void main(String[] args) {
-		new _06_IPodShuffle();
-		
+		new _06_IPodShuffle();	
 	}
 
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		// TODO Auto-generated method stub
 		if(e.getSource() == playButton) {
+			if(selectedSongIndex != -1) {
+				songList.get(selectedSongIndex).stop();
+			}
 			
+			selectedSongIndex = rand.nextInt(songList.size());
+			songList.get(selectedSongIndex).play();
+		}
+		else if(e.getSource() == stopButton) {
+			if(selectedSongIndex != -1) {
+				songList.get(selectedSongIndex).stop();
+			}
 		}
 	}
 }
--- src/_02_Integer_Stack/IntegerStack.java
+++ src/_02_Integer_Stack/IntegerStack.java
@@ -2,21 +2,29 @@ package _02_Integer_Stack;
 
 public class IntegerStack {
 	//1. create a private array of integers
+	private int[] intMemberArray;
 	
 	//2. complete the constructor by initializing the member array
 	//   to a new array of length 0. This prevents null pointer exceptions.
 	public IntegerStack() {
+		intMemberArray = new int[0];
 	}
 	
 	//3. Complete the steps in the push method.
 	public void push(int v) {
 		//A. create a new array that is one element larger than the member array
+		int intArray[] = new int[intMemberArray.length];
 		
 		//B. set the last element of the new array equal to the integer passed into the method
+		intArray[intArray.length-1] = v;
 		
 		//C. iterate through the member array and copy all the elements into the new array
+		for(int i=0; i<intMemberArray.length; i++) {
+			intArray[i] = intMemberArray[i];
+		}
 		
 		//D. set the member array equal to the new array.
+		intMemberArray = intArray;
 		
 	}
 	
